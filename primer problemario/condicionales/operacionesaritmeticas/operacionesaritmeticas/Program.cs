f748080fd72bc8829eaea6b9de137932747b9095
Euclides Marin <marinm80@hotmail.com>
2017-10-26 19:23:31 -0400

    Add files via upload

---
+++ primer problemario/condicionales/operacionesaritmeticas/operacionesaritmeticas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operacionesaritmeticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer numero");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+
+
+            if (num1 > 0)
+            {
+                Console.WriteLine("Ingrese el segundo numero");
+                int num2 = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("La suma es :" + (num1 + num2));
+                Console.WriteLine("Lamultiplicacion es :" + (num1 * num2));
+                Console.WriteLine("La division es :" + (num1 / num2));
+
+            }
+            else
+            {
+                Console.Write("No se admiten numeros negativos");
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
